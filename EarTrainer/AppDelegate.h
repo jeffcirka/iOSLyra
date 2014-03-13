8850e475d6630a8416b99d44f9ec98271eb2c8c6
Jeff Cirka <jcirkactr@Jeffs-MacBook-Pro.local>
2014-03-12 21:38:21 -0700

    Initial Commit

---
+++ EarTrainer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EarTrainer
+//
+//  Created by Jeff Cirka on 3/12/14.
+//  Copyright (c) 2014 Lyra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
